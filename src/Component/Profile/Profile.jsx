6092cefa43a8e3b3c6a2e82460b134ffa735d7c2
momen210 <touristmomen@gmail.com>
2024-01-29 23:56:03 +0600

    profile section added

--- src/Component/Profile/Profile.jsx
+++ src/Component/Profile/Profile.jsx
@@ -1,9 +1,29 @@
+import { useContext } from "react";
+import { AuthContext } from "../../AuthProvider/AuthProvider";
+
 const Profile = () => {
-    return (
+  const { user } = useContext(AuthContext);
+  const { displayName, email, emailVerified, photoURL } = user;
+
+  return (
+    <div className="max-w-6xl mx-auto">
+      <div className="my-12 flex justify-between">
         <div>
-            
+          <h1 className="mb-4 text-xl font-semibold">{displayName}</h1>
+          <img src={photoURL} alt="" />
         </div>
-    );
+        <div>
+          <p className="text-xl font-semibold mb-3">Your appointment:</p>
+          <p>
+            {emailVerified
+              ? "Thanks for verifying your email"
+              : "Verify your email please!"}
+          </p>
+        </div>
+        <p className="text-xl font-semibold">{email ? email : "No email?!"}</p>
+      </div>
+    </div>
+  );
 };
 
 export default Profile;
