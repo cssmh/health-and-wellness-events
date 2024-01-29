08020ca3dd50c88685101bd31042f55e68a63fbd
momen210 <touristmomen@gmail.com>
2024-01-29 11:55:40 +0600

    first commit

---
+++ src/Router/Router.jsx
@@ -0,0 +1,13 @@
+import { createBrowserRouter } from "react-router-dom";
+
+const Router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello world!</div>,
+  },
+]);
+
+export default Router;
+
+
+{/* <div class="flex mt-10 aos-init aos-animate" data-aos="fade-up" data-aos-delay="400" data-aos-offset="100"></div> */}
