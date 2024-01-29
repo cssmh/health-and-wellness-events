2c57099a00ee5645d1cd933593e0d938c5a17a84
momen210 <touristmomen@gmail.com>
2024-01-29 15:45:59 +0600

    fake data 8th img url changes, card btn flex-grow added"

--- src/Component/Home/CardServices.jsx
+++ src/Component/Home/CardServices.jsx
@@ -1,18 +1,25 @@
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 const CardServices = ({ getServices }) => {
-    console.log(getServices);
-    const {name, description, image_url } = getServices
-    return (
-        <div className="border border-gray-300 p-5 rounded-lg">
-            <h1 className="text-xl text-custom-blue mb-2 font-semibold text-center">{name}</h1>
-            <img src={image_url} alt="" />
-            <p className='mt-2 text-gray-500'>{description}</p>
-        </div>
-    );
+  console.log(getServices);
+  const { name, description, image_url } = getServices;
+  return (
+    <div className="border border-gray-300 p-5 rounded-lg flex flex-col">
+      <div className="flex-grow">
+      <h1 className="text-xl text-custom-blue mb-2 font-semibold text-center">
+        {name}
+      </h1>
+      <img src={image_url} alt="" />
+      <p className="my-2 text-gray-500">{description}</p>
+      </div>
+      <button className="bg-custom-blue w-full text-white py-1 rounded-md">
+        Choose {name}
+      </button>
+    </div>
+  );
 };
 
 CardServices.propTypes = {
-    getServices: PropTypes.object
-}
+  getServices: PropTypes.object,
+};
 
 export default CardServices;
