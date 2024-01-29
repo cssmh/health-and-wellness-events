9d56f9946c4ce509f94e297765e97af33dd527a4
momen210 <touristmomen@gmail.com>
2024-01-29 14:12:23 +0600

    navbar design completed

---
+++ src/Component/Navbar/Navbar.jsx
@@ -0,0 +1,81 @@
+import navLogo from "../../assets/iconmain.png";
+import { Link, NavLink } from "react-router-dom";
+
+const Navbar = () => {
+  const allNav = (
+    <>
+      <NavLink
+        to={"/"}
+        className={({ isActive }) =>
+          isActive
+            ? "bg-green-400 text-white px-2 py-1 rounded-md"
+            : "px-2 py-1 rounded-md"
+        }
+      >
+        Home
+      </NavLink>
+      <NavLink
+        to={"/profile"}
+        className={({ isActive }) =>
+          isActive
+            ? "bg-green-400 text-white px-2 py-1 rounded-md"
+            : "px-2 py-1 rounded-md"
+        }
+      >
+        Profile
+      </NavLink>
+      <NavLink
+        to={"/services"}
+        className={({ isActive }) =>
+          isActive
+            ? "bg-green-400 text-white px-2 py-1 rounded-md"
+            : "px-2 py-1 rounded-md"
+        }
+      >
+        Services
+      </NavLink>
+    </>
+  );
+
+  return (
+    <div className="navbar bg-base-300 my-4 px-7">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className=" -ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
+          </div>
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+          >
+            {allNav}
+          </ul>
+        </div>
+        <Link to={"/"} className="flex items-center text-2xl gap-2 font-semibold">
+          <img className="w-12" src={navLogo} alt="" /> MediCare
+        </Link>
+      </div>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1">{allNav}</ul>
+      </div>
+      <div className="navbar-end">
+        <a className="">Login</a>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -1,9 +1,27 @@
 import { createBrowserRouter } from "react-router-dom";
+import MainLayout from "../MainLayout/MainLayout";
+import Home from "../Component/Home/Home";
+import Profile from "../Component/Profile/Profile";
+import Services from "../Component/Services/Services";
 
 const Router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <MainLayout></MainLayout>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>
+      },
+      {
+        path: "/profile",
+        element: <Profile></Profile>
+      },
+      {
+        path: "/services",
+        element: <Services></Services>
+      },
+    ]
   },
 ]);
 
