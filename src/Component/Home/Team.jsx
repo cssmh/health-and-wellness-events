616f9cd6e741afd44ea0dbf6725f69703280111a
momen210 <touristmomen@gmail.com>
2024-01-30 15:47:43 +0600

    finally completed project, time to deploy again

--- src/Component/Home/Team.jsx
+++ src/Component/Home/Team.jsx
@@ -1,14 +1,14 @@
 import team from "../../assets/hd.png"
 const Team = () => {
     return (
-        <div className="flex flex-col lg:flex-row justify-between items-center my-10 max-w-[1200px] mx-auto bg-blue-300 rounded-lg">
+        <div data-aos="flip-down" className="flex flex-col lg:flex-row justify-between items-center my-10 max-w-[1200px] mx-auto bg-blue-300 rounded-lg">
       <div className="lg:w-3/5 pl-16 pt-8 lg:pt-0">
         <h1 className="text-5xl text-white font-semibold">
           We are always here for you!
           Trust Yourself,
           Be Strong
         </h1>
-        <p className="text-lg text-gray-800 mt-2">
+        <p className="text-lg text-gray-800 mt-2 my-3 lg:my-0">
           Thank you for trusting us.<br></br>We are at the peak of medical
           professionals today!
         </p>
